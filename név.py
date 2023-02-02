d571fd012a6bc7bb600912d059e0a74903bed18a
VorosDominik <medoxidhun@gmail.com>
2023-02-02 16:09:30 +0100

    több esett

--- név.py
+++ név.py
@@ -27,6 +27,11 @@ def teszt_esetek():
     r_gep21()
     r_gep21_21kevesebblap()
     r_tultoltes()
+    konkret_dontetlen_teszt()
+    lapok_összege_g_kisseb_21_teszt()
+    lapok_összege_j_kisseb_21_teszt()
+    nagyobb_nyer_teszt()
+    nagyobb_nyer2_teszt()
 
 def jatekos_vesztett():
     jatekos = [10, 9, 3]
@@ -64,7 +69,7 @@ def r_gep21_21kevesebblap():
 
 
 def r_tultoltes():
-    print("Játékos tul lépte 21 teszt:")
+    print("Játékos tul lépte 21 et teszt:")
     jatekos = [10, 9, 3]
     gep = [10, 9]
     vart_eredmeny = "j veszett"
@@ -73,4 +78,70 @@ def r_tultoltes():
         print("a jatekos_vesztett teszt sikeres")
     else:
         print("a jatekos_vesztett teszt megbukott")
+
+def lapok_összege_g_kisseb_21_teszt():
+    print("gep_kevesebb 21 nel")
+    jatekos = [10, 9, 2]
+    gep = [10, 9]
+    vart_eredmeny = "gep veszett"
+    kapott_eredmeny = eredemeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("a jatekos_vesztett teszt sikeres")
+    else:
+        print("a jatekos_vesztett teszt megbukott")
+def lapok_összege_j_kisseb_21_teszt():
+    print("j_kevesebb 21 nel")
+    jatekos = [10, 2, 2]
+    gep = [10, 9]
+    vart_eredmeny = "j veszett"
+    kapott_eredmeny = eredemeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("a jatekos_vesztett teszt sikeres")
+    else:
+        print("a jatekos_vesztett teszt megbukott")
+
+def nagyobb_nyer_teszt():
+    print("gep nagyobb mint jatekos")
+    jatekos = [10, 2, 2]
+    gep = [10, 9]
+
+    kapott_eredmeny = eredemeny(jatekos, gep)
+    if gep>jatekos:
+    vart_eredmeny = "j veszett"
+    if kapott_eredmeny == vart_eredmeny:
+        print("a jatekos_vesztett teszt sikeres")
+    else:
+        print("a jatekos_vesztett teszt megbukott")
+
+
+
+def nagyobb_nyer2_teszt():
+    print("gep kissebb mint jatekos")
+    jatekos = [10, 2, 2]
+    gep = [10, 9]
+
+    kapott_eredmeny = eredemeny(jatekos, gep)
+    if gep < jatekos:
+        vart_eredmeny = "gep veszett"
+    if kapott_eredmeny == vart_eredmeny:
+        print("a jatekos_vesztett teszt sikeres")
+    else:
+        print("a jatekos_vesztett teszt megbukott")
+
+
+def konkret_dontetlen_teszt():
+    print("ugyan anyi lap, mind ketö teljesen egyenlöen vesztett vagy nyert")
+    jatekos = [10, 9, 2]
+    gep = [10, 9, 2]
+
+    kapott_eredmeny = eredemeny(jatekos, gep)
+    if gep == jatekos:
+        vart_eredmeny = "j veszett"
+    if kapott_eredmeny == vart_eredmeny:
+        print("a jatekos_vesztett teszt sikeres")
+    else:
+        print("a jatekos_vesztett teszt megbukott")
+
+
+
 teszt_esetek()
